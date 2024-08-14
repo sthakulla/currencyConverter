1c77f6f388799a9e7e759dcf12cae8e6ddefd7ca
Subham <39816945+sthakulla@users.noreply.github.com>
2024-08-14 14:08:55 -0700

    Finishing project to convert currency

--- currencyConverter.py
+++ currencyConverter.py
@@ -1,4 +1,36 @@
+import requests
+import config
 
+def convert_currency():
+    init_currency = input('Enter an initial currency: ')
+    target_currency = input('Enter a target currency: ')
 
-initialMoney = input('Enter amount to convert to Nepalese Currency:')
+    while True:
+        try:
+            amount = float(input('Enter the amount: '))
+        except:
+            print('The amount must be a numeric value!')
+            continue
 
+        if not amount > 0:
+            print('The amount must be greater than 0')
+            continue
+        else:
+            break
+
+    url = "https://api.apilayer.com/fixer/convert?to="+target_currency+"&from="+init_currency+"&amount="+str(amount)
+    
+    payload = {}
+    headers = {'apikey': config.API_KEY}
+    response = requests.request('GET', url, headers=headers, data=payload)
+    status_code = response.status_code
+    
+    if  status_code!=200:
+        print ("Error! There was a problem. Please try again!")
+        quit()
+        
+    result = response.json()
+    print("Conversion result: "+ str(result['result']))
+    
+if __name__ == '__main__':
+    convert_currency()
